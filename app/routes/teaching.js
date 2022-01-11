5b9f2fb3a0f3fe325c7c9250d603042c84f37813
Shane <sgoneill89@gmail.com>
2022-01-11 15:04:41 +0100

    added new routes and developed nav

--- app/routes/teaching.js
+++ app/routes/teaching.js
@@ -1,4 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  setupController(controller) {
+    this._super(...arguments);
+    // controller.set('isMobile', this.media.isMobile),
+    controller.set('header', "header")
+    this._checkScroll(controller);
+  },
+
+  _checkScroll(controller) {
+    window.addEventListener('scroll', (() => {
+      let scrollPosition = Math.round(window.scrollY);
+      if (scrollPosition > 90){
+        controller.set('header', "sticky");
+      }
+      else {
+        controller.set('header', "header");
+      }
+    }));
+  }
 });
