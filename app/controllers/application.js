5da91e8f0f82a7b8f3361f25d2859ad9b16909c9
Shane <sgoneill89@gmail.com>
2022-01-13 16:25:37 +0100

    moved listeners to application controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,6 +6,63 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   intl: service(),
 
+  loadListener: window.addEventListener('load', () => {
+    let navItemActive;
+    var navItem1 = document.getElementById('nav-link-1');
+    if (navItem1.className.includes("active")) {
+      navItem1.style.color = '#edd5ea';
+      navItem1.style.backgroundColor = '#61505f';
+      navItem1.style.boxShadow = '0px -1px 5px #888888';
+    }
+    var navItem2 = document.getElementById('nav-link-2');
+    if (navItem2.className.includes("active")) {
+      navItem2.style.color = '#edd5ea';
+      navItem2.style.backgroundColor = '#61505f';
+      navItem2.style.boxShadow = '0px -1px 5px #888888';
+    }
+    var navItem3 = document.getElementById('nav-link-3');
+    if (navItem3.className.includes("active")) {
+      navItem3.style.color = '#edd5ea';
+      navItem3.style.backgroundColor = '#61505f';
+      navItem3.style.boxShadow = '0px -1px 5px #888888';
+    }
+    var navItem4 = document.getElementById('nav-link-4');
+    if (navItem4.className.includes("active")) {
+      navItem4.style.color = '#edd5ea';
+      navItem4.style.backgroundColor = '#61505f';
+      navItem4.style.boxShadow = '0px -1px 5px #888888';
+    }
+
+  }, false),
+
+  clickListener: document.body.addEventListener('click', () => {
+    let navItemActive;
+    var navItem1 = document.getElementById('nav-link-1');
+    if (navItem1.className.includes("active")) {
+      navItem1.style.color = '#edd5ea';
+      navItem1.style.backgroundColor = '#61505f';
+      navItem1.style.boxShadow = '0px -1px 5px #888888';
+    }
+    var navItem2 = document.getElementById('nav-link-2');
+    if (navItem2.className.includes("active")) {
+      navItem2.style.color = '#edd5ea';
+      navItem2.style.backgroundColor = '#61505f';
+      navItem2.style.boxShadow = '0px -1px 5px #888888';
+    }
+    var navItem3 = document.getElementById('nav-link-3');
+    if (navItem3.className.includes("active")) {
+      navItem3.style.color = '#edd5ea';
+      navItem3.style.backgroundColor = '#61505f';
+      navItem3.style.boxShadow = '0px -1px 5px #888888';
+    }
+    var navItem4 = document.getElementById('nav-link-4');
+    if (navItem4.className.includes("active")) {
+      navItem4.style.color = '#edd5ea';
+      navItem4.style.backgroundColor = '#61505f';
+      navItem4.style.boxShadow = '0px -1px 5px #888888';
+    }
+  }, false),
+
   actions: {
     changeLanguage(language) {
       let locale;
